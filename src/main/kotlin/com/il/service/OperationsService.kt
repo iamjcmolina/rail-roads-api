05976929e36c9af77f683472cd3b8e1327d119bf
Ricardo Reyes Varela <rreyesvar@gmail.com>
2022-06-02 11:35:28 -0500

    fix order

--- src/main/kotlin/com/il/service/OperationsService.kt
+++ src/main/kotlin/com/il/service/OperationsService.kt
@@ -1,20 +1,36 @@
 package com.il.service
 
+import com.il.model.Car
 import com.il.model.InputCars
 import com.il.model.OutputCar
 import jakarta.inject.Singleton
 
 @Singleton
 class OperationsService {
-    
+
     fun categorizeByDestinationAndReceiver(inputCars: InputCars): List<OutputCar>? {
-        val destinations = inputCars.cars.map { car -> car.destination }.toSet()
-        val receivers = inputCars.cars.map { car -> car.receiver }.toSet()
-        val outputCars = inputCars
-            .cars?.filterNot { it.nameOfCar.equals("Engine") }
-            ?.map { OutputCar(destinations.indexOf(it.destination) + 1, it.nameOfCar, it.destination, it.receiver) }
-            ?.sortedWith(compareBy<OutputCar>({ destinations.indexOf(it.destination) }).thenBy { receivers.indexOf(it.receiver) })
-        
-        return outputCars;
+
+        val orderByDestinationReceiver = mutableListOf<OutputCar>()
+
+        inputCars.destinations.forEachIndexed { index, orderDestination ->
+            val destination = mutableListOf<Car>()
+
+            inputCars.cars.forEach{
+                if (it.destination == orderDestination) {
+                    destination.add(it)
+                }
+            }
+
+            inputCars.receivers.forEach{ orderReceiver ->
+                destination.forEach{ x ->
+                    if(x.receiver == orderReceiver) {
+                        orderByDestinationReceiver.add(
+                                OutputCar(nameOfCar = x.nameOfCar, destination = x.destination, receiver = x.receiver, classificationTrack = index + 1)
+                        )
+                    }
+                }
+            }
+        }
+        return orderByDestinationReceiver
     }
 }
